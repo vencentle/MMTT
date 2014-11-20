d061fc6dafec1798fb1de68a3182b7c186b0f55b
liwenxin <liwenxin@youku.com>
2014-11-20 15:34:18 +0800

    create proj

---
+++ MMTT/MMTT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMTT
+//
+//  Created by liwenxin on 14/11/20.
+//  Copyright (c) 2014年 liwenxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MMTT/MMTT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MMTT
+//
+//  Created by liwenxin on 14/11/20.
+//  Copyright (c) 2014年 liwenxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
